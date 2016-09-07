1e06dfcf76b462ec244f5a919bfe8c128f5d8cad
LSYing <312458788@qq.com>
2016-09-07 18:55:14 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/filter/AnotherActivity.java
@@ -0,0 +1,21 @@
+package cn.edu.bistu.cs.se.filter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by lsy on 2016/9/7.
+ */
+public class AnotherActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.another_layout);
+
+    }
+}
